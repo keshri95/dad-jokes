bc3d8d1c1f570db22a913398607c310a574c0a63
Ashish Keshri <69101383+keshri95@users.noreply.github.com>
2022-11-09 11:33:50 +0530

    error-msg-added

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const api = 'https://icanhazdadjoke.com/';
 
 const jokes = document.querySelector("#joke");
 const updateJoke = document.querySelector("#get-joke");
-
+const err = document.querySelector('#err');
 // initailized xmlhttp request
 const xhr = new XMLHttpRequest();
 
+const showJoke = (joke) => {
+    jokes.innerText = joke;
+}
 
+// show error ----
+
+const showErr = (error) => {
+    err.style.display = 'block';
+    jokes.innerText = error;
+}
 
 const getJokes = () => {
 
@@ -16,18 +25,33 @@ const getJokes = () => {
 
     xhr.responseType = 'json';
 
-    console.log(xhr);
+    // console.log(xhr);
 
     // success callback function sent by onload-----
     const successMessage = () => {
-        console.log('success....')
+        const responseJoke = xhr.response.joke;
+        // console.log(responseJoke.joke);                                                              
+        showJoke(`${responseJoke}`);
     }
     xhr.onload = successMessage;
+
+    // on error ocuured -------
+    xhr.onerror = function (){
+        // console.error(`error dispute.....`);
+        showErr(`Something is wrong......`);
+    }
+
+
+
     // send method show success message
     xhr.send();
 }
 
-getJokes();
+// getJokes();
+// event on the btn 
+updateJoke.addEventListener('click', () => {
+    getJokes();
+});
 
 
 
